604c1892d612369c93ef56700f2b9e20575c8622
Manish Patil <manish>
2020-10-26 12:54:06 +0530

    First Code

---
+++ Documents/Git_repos/Licence Plate/1_code.py
@@ -0,0 +1,41 @@
+import cv2
+import imutils
+import numpy as np
+
+img = cv2.imread('numplate1.jpeg',cv2.IMREAD_COLOR)
+img = cv2.resize(img, (600,400) )
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+gray = cv2.bilateralFilter(gray, 13, 15, 15) 
+
+edged = cv2.Canny(gray, 30, 200) 
+contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(contours)
+contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+screenCnt = None
+
+for c in contours:
+    
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+ 
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+if screenCnt is None:
+    detected = 0
+    print ("No contour detected")
+else:
+     detected = 1
+
+if detected == 1:
+    cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
+
+mask = np.zeros(gray.shape,np.uint8)
+new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
+new_image = cv2.bitwise_and(img,img,mask=mask)
+
+cv2.imshow('new_image', new_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
